5a889977245328bdbd95a060c378218ec1277ae1
Douglas Serrano <esdouglasserrano@gmail.com>
2025-11-23 16:44:01 -0800

    A few notes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello World!");
+﻿// Console.WriteLine("Hello World!");
+
+// Console.WriteLine("Congragulations!");
+// Console.Write("You wrote your first lines of code.");
+
+// Console.WriteLine("This is the first line.");
+// Console.Write("This is the second line.");
+
+// Console.WriteLine('b');
+// Console.WriteLine(123);
+// Console.WriteLine(0.25F);
+// Console.WriteLine(2.625);
+// Console.WriteLine(12.39816m);
+// Console.WriteLine(true);
+// Console.WriteLine(false);
+
+// string firstName;
+// char userOption;
+// int gameScore;
+// decimal particlesPerMillion;
+// bool processedCustomer;
+
+// string firstName = "Bob";
+// Console.WriteLine(firstName);
+
+// var message = "Hello world!";
+// message = 10.703m;
